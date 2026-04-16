c8ae616a883e86746344ba1aa3180f84a2b668de
s1315119-a11y <s1315119@taivs.tp.edu.tw>
2026-04-16 16:09:05 +0800

    04160409

--- Assets/Scripts/ch8/GameManager.cs
+++ Assets/Scripts/ch8/GameManager.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 [DefaultExecutionOrder(-1)]
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Ball ball;
+    [Header("遊戲物件")]
+    [SerializeField] private Ball ballCopy;
     [SerializeField] private Paddle playerPaddle;
     [SerializeField] private Paddle computerPaddle;
+
+    [Header("UI 介面")]
     [SerializeField] private Text playerScoreText;
     [SerializeField] private Text computerScoreText;
 
@@ -20,7 +24,9 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R)) {
+        // 按下 R 鍵重置遊戲
+        if (Input.GetKeyDown(KeyCode.R))
+        {
             NewGame();
         }
     }
@@ -34,15 +40,28 @@ public class GameManager : MonoBehaviour
 
     public void NewRound()
     {
+        // Cancel ongoing delays to prevent double-spawning if NewRound is called rapidly
+        StopAllCoroutines();
+
         playerPaddle.ResetPosition();
         computerPaddle.ResetPosition();
-        ball.ResetPosition();
 
-        CancelInvoke();
-        Invoke(nameof(StartRound), 1f);
+        for (int i = 0; i < 2; i++)
+        {
+            Ball ball = Instantiate(ballCopy); // Use standard Instantiate
+            ball.ResetPosition();
+
+            // Start a Coroutine instead of Invoke
+            StartCoroutine(StartRoundRoutine(ball, 1f));
+        }
     }
-
-    private void StartRound()
+    // This replaces StartRound and handles the delay
+    private IEnumerator StartRoundRoutine(Ball ball, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        ball.AddStartingForce();
+    }
+    private void StartRound(Ball ball)
     {
         ball.AddStartingForce();
     }
@@ -70,5 +89,4 @@ public class GameManager : MonoBehaviour
         computerScore = score;
         computerScoreText.text = score.ToString();
     }
-
 }

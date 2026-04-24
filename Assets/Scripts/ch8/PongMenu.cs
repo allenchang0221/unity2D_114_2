cff7288fef2e09176d979ba2382e9b67127b2030
allenchang0221 <allenchang0221@gmail.com>
2026-04-24 21:13:00 +0800

    0424

--- Assets/Scripts/ch8/PongMenu.cs
+++ Assets/Scripts/ch8/PongMenu.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PongMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public static bool  isTime=true,isMany=false;
+    public Toggle TS,MS;
     void Start()
     {
-        
+    }
+
+    void ToggleValueChanged(Toggle change)
+    {
+        if (change.isOn)
+        {
+            Debug.Log("Switch is ON");
+        }
+        else
+        {
+            Debug.Log("Switch is OFF");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Add listener for when the state changes
+        MS.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(MS);
+        });
     }
 
     public void OBS()

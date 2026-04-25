60f085cc173b01be950b28d01aa726a16ddd69f0
allenchang0221 <allenchang0221@gmail.com>
2026-04-25 18:03:22 +0800

    0425

--- Assets/Scripts/ch8/PongMenu.cs
+++ Assets/Scripts/ch8/PongMenu.cs
@@ -7,22 +7,15 @@ using UnityEngine.UI;
 public class PongMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static bool  isTime=true,isMany=false;
-    public Toggle TS,MS;
+    public static bool isTime=true,isMany=false;
+    public Toggle TS;
     void Start()
     {
     }
 
     void ToggleValueChanged(Toggle change)
     {
-        if (change.isOn)
-        {
-            Debug.Log("Switch is ON");
-        }
-        else
-        {
-            Debug.Log("Switch is OFF");
-        }
+        isTime = change.isOn;
     }
 
     // Update is called once per frame
@@ -30,38 +23,13 @@ public class PongMenu : MonoBehaviour
     {
 
         // Add listener for when the state changes
-        MS.onValueChanged.AddListener(delegate {
-            ToggleValueChanged(MS);
-        });
-        // Add listener for when the state changes
         TS.onValueChanged.AddListener(delegate {
             ToggleValueChanged(TS);
         });
     }
 
-    public void OBS()
+    public void startGame()
     {
         SceneManager.LoadScene("Pong");
-        isTime = false;
-        isMany = false;
-    }
-    public void MBS()
-    {
-        SceneManager.LoadScene("Pong");
-        isTime = false;
-        isMany = true;
-    }
-    public void OBT()
-    {
-        SceneManager.LoadScene("Pong");
-        isTime = true;
-        isMany = false;
-    }
-    public void MBT()
-    {
-        SceneManager.LoadScene("Pong");
-        isTime = true;
-        isMany = true;
-
     }
 }

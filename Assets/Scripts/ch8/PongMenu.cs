bd33b41f50388f78f04c3adfa0621c01ac9cf8d3
allenchang0221 <allenchang0221@gmail.com>
2026-04-27 19:13:59 +0800

    0427

--- Assets/Scripts/ch8/PongMenu.cs
+++ Assets/Scripts/ch8/PongMenu.cs
@@ -8,16 +8,17 @@ using UnityEngine.UI;
 public class PongMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static bool isTime=true,isBlock;
+    public static bool isTime=true,isBlock=true;
     public static int ballNums=1;
-    public Toggle TS;
+    public Toggle TS,BS;
     public Slider ballNumSlider;
     public TextMeshProUGUI showLT;
     void Start()
     {
         Application.targetFrameRate = 60;
-        isTime = true;
-        ballNums = 1;
+        TS.isOn=isTime;
+        BS.isOn=isBlock;
+        ballNumSlider.value=ballNums;
         showLT.text = "limmit time : " +ballNums.ToString();
     }
 
--- Assets/Scripts/ch8/makeBall.cs
+++ Assets/Scripts/ch8/makeBall.cs
@@ -5,25 +5,36 @@ using UnityEngine;
 public class makeBall : MonoBehaviour
 {
     //public static dynamic b;
-    public GameObject ball,Block;
+    public GameObject ball, Block;
+    GameObject b;
+    int a = 0;
     // Start is called before the first frame update
     void Start()
     {
+        a = 0;
         for (int i = 0; i < PongMenu.ballNums; i++)
-            {
-                GameObject a = GameObject.Instantiate(ball);
-                //b[i] = a.transform.position.y;
-            }
+        {
+            GameObject a = GameObject.Instantiate(ball);
+            //b[i] = a.transform.position.y;
+        }
         if (PongMenu.isBlock)
         {
-            GameObject b = GameObject.Instantiate(Block);
-            b.transform.position = new Vector2(Random.Range(5,-5),Random.Range(-3,3));
+            b = GameObject.Instantiate(Block);
+            b.transform.position = new Vector2(Random.Range(5, -5), Random.Range(-3, 3));
+            //b.transform.position=new Vector2 (0,0);
         }
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        a++;
+        if (a % 300 == 0)
+        {
+            b.transform.position = new Vector2(Random.Range(5, -5), Random.Range(-3, 3));
+
+        }
+
     }
 }
